6eba0761315b22bce0e3ab75dc764d57ec4667ea
IsamarDavilaG <isamar.dvgz@gmail.com>
2017-01-13 10:33:46 -0500

    Actualizacion

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('form-signin-heading').innerHTML="Por favor iniciar sesión";
+document.getElementById('inputEmail').placeholder="Correo Electronico";
+document.getElementById('inputPassword').placeholder="Contraseña";
+document.querySelector('span').innerHTML="Recordar datos";
+document.querySelector('button').innerHTML="Iniciar Sesión";
+
